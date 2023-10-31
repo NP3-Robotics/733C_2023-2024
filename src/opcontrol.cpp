31fbfd50fccdb6d729c49a7b07c82b1d1df9670e
nnapper <alvinlyao@gmail.com>
2023-10-31 16:11:07 -0700

    Updating opcontrol

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -8,46 +8,33 @@
 
 using namespace std;
 
-// what each button on the controller does
 void controllerFunc(pros::Controller controller, double &speed, int &catapos, bool &wingState)
 {
-    // what the buttons are, may delete
-    bool speedUp = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP);
-    bool slowDown = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN);
+    bool cataPosition = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP);
+    bool robotHeading = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN);
+    bool changeDirection = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1);
 
     bool goForward = controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
     bool goBackward = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
 
     double turn = pow(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X) / 127, 3);
+    bool turnSmall = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X) / abs(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X));
 
     bool wingButton = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1);
 
-    bool cataButton = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
+    bool cataButton = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A);
+    bool manual = controller.get_digital(pros::E_CONTROLLER_DIGITAL_B);
 
     bool intakeIn = controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT);
     bool intakeOut = controller.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT);
 
-    bool changeDirection = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X);
+    bool distanceTravelled = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X);
 
-    bool runPIDTest = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B);
+    bool resetCata = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_Y);
 
-    bool cataPosition = controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1);
-
-    // change speed of bot, bounds make sure the bot doesn't go too fast or too slow
-    if (speedUp && speed < 1)
-    {
-        speed += 0.1;
-    }
-    if (slowDown && speed > 0.2)
-    {
-        speed -= 0.1;
-    }
-
-    // change direction of input (foward to backward or backward to foward)
     if (changeDirection)
         speed *= -1;
 
-    // foward/backward control
     if (goForward)
     {
         leftMtrs.move(127 * speed);
@@ -64,116 +51,101 @@ void controllerFunc(pros::Controller controller, double &speed, int &catapos, bo
         rightMtrs.move(0);
     }
 
-    // check for turn input
     if (turn != 0)
     {
         leftMtrs.move(127 * turn);
         rightMtrs.move(-127 * turn);
     }
 
-    // use wing
+    if (turnSmall != 0)
+    {
+        leftMtrs.move(50 * controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X) / abs(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X)));
+        rightMtrs.move(-50 * controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X) / abs(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X)));
+        // cout << "Turn input " << turnSmall << endl;
+        // cout << "Controller input " << controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X) / abs(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X)) << endl;
+    }
+
     if (wingButton)
     {
-
         if (wingState)
         {
-            // if wing open close
             wingLeft.set_value(false);
             wingRight.set_value(false);
         }
         else
         {
-            // if wing close open
+
             wingLeft.set_value(true);
             wingRight.set_value(true);
         }
 
-        // change state of wing so it can retract or extend on next press
         wingState = !wingState;
     }
 
-    // moving catapult
     if (cataButton)
     {
-        // 11 per shot
-        // 7 shaved gear per shot
-        // 110 deg for shot, 180 for next
+        double finalPos = catapult.get_position() + 185;
 
-        catapult.move_relative(180, 40);
-        // catapult.move_velocity(40);
+        catapult.move_absolute(finalPos, 35);
+
+        int t = 0;
+
+        while (!(catapult.get_position() < finalPos + 5 && catapult.get_position() > finalPos - 5))
+        {
+            cout << catapult.get_position() << endl;
+            t += 2;
+            if (t > 5000)
+                break;
+            pros::delay(2);
+        }
+    }
+    else if (manual)
+    {
+        catapult.move_velocity(35);
     }
     else
-        catapult.move(0);
+        catapult.move_velocity(0);
 
-    // moving intake
+    if (resetCata)
+    {
+        double finalPos = 135;
+        if ((catapult.get_position() - 135) / 180 <= abs(0.5))
+        {
+            while (finalPos < catapult.get_position())
+            {
+                finalPos += 180;
+            }
+        }
+        catapult.move_absolute(finalPos, 35);
+
+        while (!(catapult.get_position() < finalPos + 5 && catapult.get_position() > finalPos - 5))
+        {
+            cout << catapult.get_position() << endl;
+            pros::delay(2);
+        }
+    }
+
     if (intakeIn)
         intake.move(127);
     else if (intakeOut)
         intake.move(-127);
     else
         intake.move(0);
-
-    // meant to see how far cata needs to move, haven't tested yet
-    if (cataPosition)
-    {
-        if (cataArmMove)
-        {
-            catapos = catapult.get_position();
-        }
-        else
-        {
-            cout << catapult.get_position() - catapos << endl;
-        }
-    }
-
-    // for testing if PID is accurate or needs more tuning
-    if (runPIDTest)
-        autonomous();
 }
 
 void opcontrol()
 {
-    // declaring controller
     pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
-    // variables needed for controllerFunc()
     double speed = 1;
     int catapos = 0;
     double lastHeading = 0;
 
-    // think this was for checking inertial drift
-    double firstHeading = inertial.get_heading();
-
-    double totalTimeInOp = 0;
-    double totalInertialDif = 0;
-
-    double newInertialReading;
-    double inertialDif;
-
     while (true)
     {
-        // seeing if inertial was done calibrating
-        if (!inertial.is_calibrating())
-        {
-            newInertialReading = inertial.get_heading();
-            inertialDif = (newInertialReading - lastHeading) / 10;
-
-            totalInertialDif += totalInertialDif - firstHeading;
-            lastHeading = newInertialReading;
-
-            totalTimeInOp += 10;
-
-            // cout << "totalInertialDif is " << totalInertialDif << endl;
-            // cout << totalInertialDif / totalTimeInOp << endl;
-        }
-
-        // check for drift
-        pros::lcd::set_text(1, std::to_string(newInertialReading));
-        pros::lcd::set_text(2, std::to_string(inertialDif));
-        pros::lcd::set_text(3, std::to_string(totalInertialDif));
-
-        // controller functions
         controllerFunc(controller, speed, catapos, wingState);
+
+        cout << "Temperature is " << catapult.get_temperature() << endl;
 
         pros::delay(10);
     }
